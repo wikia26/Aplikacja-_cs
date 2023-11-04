29943cc3c43b0e33bc3031b0f7acf24ed8c0eaf7
wikia26 <w.anczykowska@gmail.com>
2023-11-04 09:41:21 +0100

    Zmiany  1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    /// <summary>
+    /// Procedura główna aplikacji
+    /// </summary>
+    /// <param name="args">Parametry wejściowe w postaci tablicy tekstowej</param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello,World!");
+        Kielbasa();
+        Console.ReadLine();
+    }
+
+    ///<summary>
+    ///
+    ///</summary>
+
+    private static void Kielbasa()
+    {
+        Console.WriteLine($"Kielbasa");
+        Console.ReadLine();
+    }
+
+}
